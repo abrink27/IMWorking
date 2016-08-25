118abebdf612c85be9999b768de37325589dc1fb
Andrew Brink <abrink27@gmail.com>
2016-08-25 23:13:29 +0000

    initial commit:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imworking::Application.config.secret_key_base = '2704004b47562ee73b9474294c7de1bc2d068595a9345779d8555aa10dc907eacd04d54248523fb04057efaa5e8d82b52a0032ab2ff9569255eed2465575da06'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Imworking::Application.config.session_store :cookie_store, key: '_imworking_session'
